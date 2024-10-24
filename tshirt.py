24bd0e4e2b4b072d06d13a0a67ea421686d5578a
zainabbasi38 <zainabbasi7534@gmail.com>
2024-10-23 23:15:32 -0700

    add

---
+++ tshirt.py
@@ -0,0 +1,8 @@
+def tshirt(size, colour, design):
+    print(f"Please enter your size: {size}")
+    print(f"Enter your colour: {colour}")
+    print(f"What you want to print on the t-shirt: {design}")
+    print("Thanks for your order!")
+
+# Example usage
+tshirt("Small", "Black", "Islamabad")
